8e1897c8729b0cbddeb780dd53957267c833c44d
Keenan06 <78598120+Keenan06@users.noreply.github.com>
2024-01-22 16:56:04 +0200

    Add files via upload

---
+++ Lesson3/src/ex03_2_exercise/ShoppingCart.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex03_2_exercise;
+
+/**
+ *
+ * @author keenanpienaar
+ */
+public class ShoppingCart {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the shopping cart");
+    }
+}
